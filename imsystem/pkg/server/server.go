5570c38499535aae47487fd17328eafeeafda2eb
hlozc <lzican@126.com>
2024-11-17 12:56:01 +0800

    广播用户发送的消息

--- imsystem/pkg/server/server.go
+++ imsystem/pkg/server/server.go
@@ -1,7 +1,12 @@
 package server
 
+// 每个连接（User）启动的时候，都有协程监听管道，管道里面的数据就是要发送给客户端的数据
+// 服务器的管道用来广播给所有的 User
+// 每个连接（User）启动的时候，也要启动一个协程监听该连接的所有读事件，有数据就读取出来并广播
+
 import (
 	"fmt"
+	"io"
 	"log/slog"
 	"net"
 	"sync"
@@ -49,7 +54,39 @@ func (s *Server) listenMessages() {
 	}
 }
 
+// 监听这个连接是否有发送数据过来
+func (s *Server) listenConn(user *User, conn net.Conn) {
+	buffer := make([]byte, 4096)
+	for {
+		n, err := conn.Read(buffer)
+		// 说明对方关闭连接了
+		if n == 0 {
+			s.broatcast(user, "offline")
+			return
+		}
+
+		// 如果有错误，但是并不是正常读取到 EOF
+		if err != nil && err != io.EOF {
+			fmt.Println("Connect Error")
+			return
+		}
+
+		// 去掉最后的 '\n', 得到用户发送过来的数据
+		msg := string(buffer[:n])
+		if msg[n-1] == '\n' {
+			msg = msg[:n-1]
+		}
+		if n > 2 && msg[n-1] == '\r' {
+			msg = msg[:n-1]
+		}
+
+		s.broatcast(user, msg)
+	}
+}
+
 // 处理该连接的所有事件
+// 其实做的事情就是：新连接到来了，为他做准备工具，然后广播其他用户，现在有一个新用户上线了
+// 随后进入 select 准备随时处理该连接后续的所有操作
 func (s *Server) handle(conn net.Conn) {
 	user := NewUser(conn)
 
@@ -58,7 +95,11 @@ func (s *Server) handle(conn net.Conn) {
 	s.onlineMap[user.Name] = user
 	s.mapLock.Unlock()
 
+	// 通知其他用户现在有新用户上线了
 	s.broatcast(user, "online")
+
+	// 还要监听这个新连接是否有数据到来
+	go s.listenConn(user, conn)
 
 	select {}
 }
